ac2b6f64e033da3df892afc31ba3cfc6eaf530b4
nroh555 <nroh555@aucklanduni.ac.nz>
2024-04-10 20:52:13 +1200

    Added line graph to visualise data

---
+++ app.py
@@ -0,0 +1,43 @@
+from dash import Dash, dcc, html
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__)
+
+colors = {
+    'background': '#FFFFFF',
+    'text': '#000000'
+}
+
+# Read the CSV files
+df1 = pd.read_csv("data/output.csv")
+
+df = pd.DataFrame({
+    "Year": df1['date'],
+    "Sales": df1['sales']
+})
+
+fig = px.line(df, x="Year", y="Sales")
+
+fig.update_layout(
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text']
+)
+
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+    html.H1(
+        children='Sales of Pink Morsel Before and After Price Increase on 15th January 2021',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
+    dcc.Graph(
+        id='example-graph-2',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
