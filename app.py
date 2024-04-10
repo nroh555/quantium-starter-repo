1506eccadf17ef87b49a267898a7b51cf5b6b10a
nroh555 <nroh555@aucklanduni.ac.nz>
2024-04-10 23:18:13 +1200

    Added radio functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, Input, Output, callback
 import plotly.express as px
 import pandas as pd
 
@@ -15,10 +15,11 @@ df1 = df1.sort_values(by="date")
 
 df = pd.DataFrame({
     "Year": df1['date'],
-    "Sales": df1['sales']
+    "Sales": df1['sales'],
+    "Region": df1['region']
 })
 
-fig = px.line(df, x="Year", y="Sales")
+fig = px.line(df, x="Year", y="Sales", color="Region")
 
 fig.update_layout(
     plot_bgcolor=colors['background'],
@@ -35,10 +36,38 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
         }
     ),
     dcc.Graph(
-        id='example-graph-2',
+        id='line-graph',
         figure=fig
-    )
+    ),
+    html.Br(),
+    html.Label('Select Region', style={'textAlign': 'center', 'color': colors['text']}),
+    dcc.RadioItems(id='regions-radio', options=[
+        {'label': 'North', 'value': 'north'},
+        {'label': 'East', 'value': 'east'},
+        {'label': 'South', 'value': 'south'},
+        {'label': 'West', 'value': 'west'},
+        {'label': 'All', 'value': 'All'}
+    ], value='All'),
 ])
+
+
+@callback(
+    Output('line-graph', 'figure'),
+    Input('regions-radio', 'value'))
+def update_graph(selected_region):
+    if selected_region == 'All':
+        df_filtered = df1
+    else:
+        df_filtered = df1[df1['region'] == selected_region]
+
+    # Plotting
+    fig = px.line(df_filtered, x="date", y="sales", color="region")
+    fig.update_layout(
+        plot_bgcolor=colors['background'],
+        paper_bgcolor=colors['background'],
+        font_color=colors['text']
+    )
+    return fig
 
 if __name__ == '__main__':
     app.run(debug=True)
